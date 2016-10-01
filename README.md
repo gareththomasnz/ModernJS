b495b717929c7b6fc6185fc81cedc8511e8ca9c1
gareththomasnz <gareththomasnz@yahoo.com>
2016-10-02 08:02:58 +1300

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# ModernJavascript
+
+Udemy broad JS course
+
+## Built With
+
+Komodo - hand coding exercise
+
+## Authors
+
+Gareth Thomas
+
+## License
+
+This is coded from a commercial course - not public domain
+
+## Acknowledgments
+
+Udemy 
+
+## Comment
+
+Covers Node, NPM, Bower, Grunt, LESS, require etc
---
+++ calculator.js
@@ -0,0 +1,24 @@
+console.log("FROM CALC");
+
+//module.exports.add = function(left, right){
+//  return left + right;      
+//};
+//
+//module.exports.sub = function(left, right){
+//  return left - right;      
+//};
+
+//module.exports = {
+//        add: function(left, right){  return left + right;},
+//        sub: function(left, right){  return left - right;}
+//};
+
+//exports.mult = function(left, right){
+//  return left * right;
+//};
+
+exports.add = add;
+
+function add(left, right){
+        return left + right;
+}
